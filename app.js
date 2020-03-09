b7ee018326d82db41d4981b1cfa374bf528a04ba
Tom Palmer <tom@localhost.localdomain>
2020-03-09 13:32:40 +0000

    Verbose comments && todo list

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
-const SimpleNodeLogger = require('simple-node-logger'),
-    opts = {
-        logFilePath:'/var/log/cheesecake456.log',
-        timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
-    },
+/* ----------------- Declare && initialise required packages ---------------- */
+	const fs = require('fs');
+	const Discord = require('discord.js');
+	const { prefix, token } = require('./config.json');
+	const SimpleNodeLogger = require('simple-node-logger'),
+	opts = {
+		logFilePath:'/var/log/cheesecake456.log',
+		timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
+	},
+	log = SimpleNodeLogger.createSimpleFileLogger( opts );
+	var nanoid = require('nanoid');
+	var process = require('process');
+	var moment = require('moment');
+	uuid = nanoid(10);
+	const client = new Discord.Client();
+	client.commands = new Discord.Collection();
+	process.stdin.resume();
+	const queue = new Map();
+/* -------------------------------------------------------------------------- */
 
-log = SimpleNodeLogger.createSimpleFileLogger( opts );
-var nanoid = require('nanoid');
-var process = require('process');
-
-var moment = require('moment');
-
-uuid = nanoid(10);
-
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-process.stdin.resume();
-const queue = new Map();
-
+//Import commands && initialise routing engine
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
+// Initialise the listener
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('=== DISCORD BOT IS RUNNING ===');
 	log.info(`[${uuid}] Connected @ [${moment().format("YYYY-MM-DD HH:mm:ss")}] as ${client.user.username}`);
 });
 
+// Execute when a "slash command" is received
 client.on('message', async (msg) => {
+	// Check the bot has been pinged with a recognised prefix
 	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
+	// Grab the arguments & command requested
 	const args = msg.content.slice(prefix.length).split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
+	// Check command exists and store in command variable
 	if (!client.commands.has(commandName)) return;
-  		const command = client.commands.get(commandName);
+  	const command = client.commands.get(commandName);
 
-  if (command.args && !args.length) {
-    return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
-  }
+	// Check if arguments are needed
+	// Prompt user for them if not specified.
+	if (command.args && !args.length) return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
 
+	// Attempt to execute specified command and log result.
+	// @TODO - Clean this shit up.
 	try {
 		command.execute(msg, args, queue);
 		log.info(`[${uuid}] Command '!${commandName}' executed by ${msg.author.username}`);
@@ -54,12 +59,14 @@ client.on('message', async (msg) => {
 		fs.appendFile('/var/log/node-error.log', `\n${moment().format("YYYY-MM-DD HH:mm:ss")} ERROR [${uuid}] Command '!${commandName}' failed with the following message: \n${err.stack}\n------------------------------------------------\n`, function () {
 			console.log(`Command '!${commandName}' failed with the following message: `, err.name);
 		}); 
-		}
+	}
 });
 
+// Process & log shutdown of bot
 process.on('SIGINT', async function(code) {  
 	await log.warn(`[${uuid}] Session terminated @ ${new Date().toJSON()} with code ${code}\n`);
 	return process.kill(process.pid);
 });
 
+// Connect to discord & start the above...
 client.login(token);
